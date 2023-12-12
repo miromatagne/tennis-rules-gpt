7dd0315a98f212cd45d441e1467dfbba8f9729ad
mmatagne <miromanuel.matagne@gmail.com>
2023-12-12 14:31:46 +0100

    test for data extractor

--- data_extractor.py
+++ data_extractor.py
@@ -4,13 +4,18 @@ from tqdm import tqdm
 import json
 
 
-def extract_files():
+def extract_files(data_path="data/PDF", metadata_path="data/metadata.json"):
     pdf_contents = {}
-    metadata = json.load(open("data/metadata.json"))
-    for file in tqdm(os.listdir("data/PDF")):
+    metadata = json.load(open(metadata_path))
+    for file in tqdm(os.listdir(data_path)):
         file_name = file.split(".pdf")[0]
         loader = UnstructuredPDFLoader(file)
         data = loader.load()
         text = data[0].page_content
         pdf_contents[file_name] = {"text": text, "doc_nb": metadata[file_name]["doc_nb"], "title": metadata[file_name]["title"], "url": metadata[file_name]["url"]}
     return pdf_contents
+
+
+if __name__ == "__main__":
+    pdf_content = extract_files()
+    print(pdf_content)
